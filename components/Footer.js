0be47181835c48d25e94e9aa9755b10a271dc6a2
Illya Klymov <xanf@xanf.me>
2021-05-20 05:47:23 +0300

    Minor tweaks

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
-  const frontendVersion = process.env.NEXT_PUBLIC_FRONTEND_VERSION ?? 'Unknown';
+  const frontendVersion =
+    process.env.NEXT_PUBLIC_FRONTEND_VERSION ?? 'undefined';
   const backendVersion =
-    process.env.BACKEND_VERSION ?? globalThis?.backendVersion ?? 'Unknown';
+    process.env.BACKEND_VERSION ?? globalThis?.backendVersion ?? 'undefined';
 
   return (
     <div>
