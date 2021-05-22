8181c2314d53deba019a1fd31b0aac91f468c791
Bohdan Zamchenco <zamchenco@gmail.com>
2021-05-22 10:39:14 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
   },
-  // globals: {
-  //   globalThis: false,
-  // },
+  globals: {
+    globalThis: false,
+  },
 };
